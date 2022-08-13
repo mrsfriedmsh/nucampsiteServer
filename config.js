63c4bb3b992d84bea7e02fceeb52152993d18f5c
mrsfriedmsh <99189153+mrsfriedmsh@users.noreply.github.com>
2022-08-13 09:18:00 -0700

    facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook' : {
+        clientId: '624113712351689',
+        clientSecret: '608d85a767c4df3c07afe1510d3341a5'
+    }
 }
